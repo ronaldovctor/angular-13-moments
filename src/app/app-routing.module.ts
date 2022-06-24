6fb7fd2984bf1291411d5aa81db3cb8fe147e2a0
Ronaldo <ronaldovctor@outlook.com>
2022-06-24 19:26:24 -0300

    Pagina individual de momento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 const routes: Routes = [
@@ -13,6 +14,9 @@ const routes: Routes = [
   },
   {
     path: 'moments/new', component: NewMomentComponent
+  },
+  {
+    path: 'moments/:id', component: MomentComponent
   }
 ];
 
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -18,6 +18,10 @@ export class MomentService {
     return this.http.get<Response<Moment[]>>(this.apiUrl)
   }
 
+  getMoment(id: number): Observable<Response<Moment>>{
+    return this.http.get<Response<Moment>>(`${this.apiUrl}/${id}`)
+  }
+
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData)
   }
