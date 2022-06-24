6a45e282a2aa38f3c2d3b079e9f1180d59a470e6
Ronaldo <ronaldovctor@outlook.com>
2022-06-24 18:53:39 -0300

    Campos de busca

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,7 +14,8 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = []
   baseApiUrl = environment.baseApiUrl
 
-  //TODO search
+  faSearch = faSearch
+  searchTerm: string = ''
 
   constructor(private momentService: MomentService) { }
 
@@ -26,6 +27,14 @@ export class HomeComponent implements OnInit {
       })
       this.allMoments = data
       this.moments = data
+    })
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement
+    const value = target.value
+    this.moments = this.allMoments.filter(moment => {
+      return moment.title.toLowerCase().includes(value)
     })
   }
 
